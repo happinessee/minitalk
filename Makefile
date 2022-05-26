ef95be9a9a09300c85700e9ff41a59e6c4b04f46
happinessee <gygur1206@gmail.com>
2022-05-26 12:30:58 +0900

    feat) Makefile

---
+++ Makefile
@@ -0,0 +1,44 @@
+NAME = client
+NAME2 = server
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+
+SRCS = 	minitalk_utils.c \
+		write_func.c \
+		bit.c
+
+SRC_CLI = ft_client.c
+
+SRC_SER = ft_server.c
+
+OBJS = $(SRCS:%.c=%.o)
+OBJ_CLI = $(SRC_CLI:%.c=%.o)
+OBJ_SER = $(SRC_SER:%.c=%.o)
+
+INCS = minitalk.h
+
+%.o : %.c
+	$(CC) $(CFLAGS) -c $< -o $@ -I .
+
+$(NAME) : $(OBJS) $(OBJ_CLI)
+	$(CC) -o $(NAME) $(CFLAGS) $(OBJS) $(OBJ_CLI)
+
+$(NAME2) : $(OBJS) $(OBJ_SER)
+	$(CC) -o $(NAME) $(CFLAGS) $(OBJS) $(OBJ_SER)
+
+all : $(NAME) $(NAME2)
+
+clean :
+	rm -f $(OBJS) $(OBJ_CLI) $(OBJ_SER)
+
+fclean : clean
+	rm -f $(NAME) $(NAME2)
+
+re :
+	make fclean
+	make all
+
+bonus : $(NAME) $(NAME2)
+
+.PHONY : all clean re bonus fclean
---
+++ bit.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bit.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyojeong <hyojeong@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 15:48:12 by hyojeong          #+#    #+#             */
+/*   Updated: 2022/05/25 15:49:39 by hyojeong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+#include <signal.h>
+#include <unistd.h>
+
+void	gather_bit(int signo, int *tmp)
+{
+	if (signo == SIGUSR1)
+	{
+		*tmp = (*tmp << 1) + 1;
+	}
+	else
+	{
+		*tmp = (*tmp << 1);
+	}
+}
+
+void	make_bit(int tmp, int pid, int cnt)
+{
+	while (--cnt != -1)
+	{
+		if ((tmp & (1 << cnt)) >> cnt)
+		{
+			kill(pid, SIGUSR1);
+		}
+		else
+		{
+			kill(pid, SIGUSR2);
+		}
+		usleep(100);
+	}
+}
